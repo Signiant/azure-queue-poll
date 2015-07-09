80f41989805a15c5739e88090b69395e353176b3
Mike Perttula <mperttula@signiant.com>
2015-07-09 10:18:25 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # azure-queue-poll
-Polls an Azure Storage Queue for messages and writes them to syslog
+Polls an Azure storage queue for messages and writes to STOUT
+
+## Config file format
+````
+{
+	"account": "AZURE STORAGE ACCOUNT NAME", 
+	"key": "AZURE STORAGE ACCOUNT KEY",
+	"queue": "AZURE STORAGE QUEUE NAME"
+}
+
+````
+Sample docker run command showing how to mount the config file into the container using a data volume on the local docker host
+:
+````
+docker run -d --privileged=true \
+              -v /my/local/folder/qbranch_config.json:/usr/src/app/qbranch_config.json \
+              --name azure_queue_poll \
+              signiant/azure-queue-poll
+````
---
+++ qbranch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# qbranch.py 1.0
+# Checks an Azure error queue
+# Author: Mike Perttula
+
+import json
+import string
+import time
+import datetime
+import argparse
+from azure import *
+from azure.storage import QueueService
+
+
+def checkQueue(aName,aKey,aQueue):
+	while 1:
+		try:
+			queue_service = QueueService(account_name=aName, account_key=aKey)
+		except:
+			print "Could not connect to queue"	
+		else:
+			try:
+				queue_metadata = queue_service.get_queue_metadata(aQueue)
+			except:
+				print "Could not find queue"
+
+			else:
+				try:
+					count = queue_metadata['x-ms-approximate-messages-count']
+					print "Count: " + count
+					if int(count) > 0:
+						messages = queue_service.get_messages(aQueue)
+						for message in messages:
+   							print(message.message_text)
+    							queue_service.delete_message(aQueue, message.message_id, message.pop_receipt)
+				except:
+					print "Could not read queue"	
+
+		time.sleep(15)
+
+parser = argparse.ArgumentParser(prog='qbranch')
+parser.add_argument('config',help="Queue config file")
+args = parser.parse_args()
+
+if args.config:
+	with open(args.config) as configFile:
+		config = json.load(configFile)
+	checkQueue(config["account"],config["key"],config["queue"])
+else:
+	parser.print_help()

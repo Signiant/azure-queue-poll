afc859dda8e3a52b1d81a922dee56c472a5bddd2
Mike Perttula <mperttula@signiant.com>
2015-07-09 15:11:00 -0400

    Fixed message spacing

--- qbranch.py
+++ qbranch.py
@@ -27,7 +27,7 @@ def checkQueue(aName,aKey,aQueue):
 			else:
 				try:
 					count = queue_metadata['x-ms-approximate-messages-count']
-					print aQueue + "queue is empty"
+					print aQueue + " queue is empty"
 					if int(count) > 0:
 						print "Messages in " + aQueue + " queue: " + count
 						messages = queue_service.get_messages(aQueue)
